0f1ee2551ed6a9142360fdeae71f050d4ed9bb8c
Bento <david.bento-pereira@lilo.org>
2024-03-13 16:40:39 +0100

    Continue work on SpotifyService + add empty spec file for the service

--- app/services/spotify_service.rb
+++ app/services/spotify_service.rb
@@ -1,48 +1,99 @@
 require 'open-uri'
 
 class SpotifyService
-  def initialize(spotify_user)
+  attr_reader :current_user, :spotify_user
+
+  def initialize(current_user, spotify_user)
+    @current_user = current_user
     @spotify_user = spotify_user
+    # TODO: Should I place the initialization of spotify_user into the service ? Or Keep receiving it from the controller???
+    # @spotify_user = spotify_user || RSpotify::User.new(current_user.spotify_data)
   end
 
-  # TODO: [Performance] I should create a background job to process this work
-  # TODO: [Performance] I should make an insert_all rather than .create
-  #
-  # TODO: [] Manage when an artist isn't followed anymore
+  # TODO: [️️️⚡️Performance] I should make an insert_all rather than a find_or_create
+  # TODO: [⚡️Performance] I should create a background job to process this work
+  # TODO: [✨DB Integrity] Rollback if ther's any problem during the process
   def load_artists
-    number_of_artists_to_load, last_artist_id = 50, nil
+    # Starts Timer
+    starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
 
-    while number_of_artists_to_load == 50
-      followed_artists = @spotify_user.following(type: 'artist', limit: 50, after: last_artist_id)
-      last_artist_id = followed_artists.last.id
-      number_of_artists_to_load = followed_artists.size
+    previously_followed_artists_by_name = @current_user.artists.pluck(:name).to_set
+    all_followed_artists = []
+    last_artist_id = nil
+    number_of_artists_to_load = 50
 
-      # Add the artists to the database
-      followed_artists.each do |artist|
-        # We make a find_or_create_by to avoid duplicates
-        # TODO: [Performance Tip] I should :
-        # - add an attribute spotify_id to Artist
-        # - create a table index on spotify_id
-        # - do the find_or_create_by on artist.spotify_id
-        # Peformance will be better by searching on an Integer than on a String
-        new_artist = Artist.find_or_create_by(name: artist.name) do |a|
-          a.name = artist.name
-          a.external_link = external_link(artist.external_urls["spotify"])
+    while number_of_artists_to_load == 50 # Why 50 ? Because it's the maximum number of elements per page we can get
+      batch_of_followed_artists = fetch_followed_artists(last_artist_id)
+      all_followed_artists << batch_of_followed_artists
+      last_artist_id = batch_of_followed_artists.last&.id
+      number_of_artists_to_load = batch_of_followed_artists.size
+
+      # We make a find_or_create_by to avoid duplicates
+      # TODO: [⚡️Performance Tip] I should :
+      # - add an attribute spotify_id to Artist
+      # - create a table index on spotify_id
+      # - do the find_or_create_by on artist.spotify_id
+      # Peformance will be better by searching on an Integer than on a String
+      batch_of_followed_artists.each do |followed_artist|
+        # Try to find the artist by name
+        artist = Artist.find_or_initialize_by(name: followed_artist.name)
+
+        # If the artist doesn't exist, create it with its external link and avatar
+        if artist.new_record?
+          artist.external_link = followed_artist.external_urls["spotify"]
+          attach_avatar(artist, followed_artist)
+          artist.save!
         end
 
-        # Attach the avatar to the artist
-        begin
-          new_artist.avatar.attach(io: open_avatar(artist.images.last["url"]), filename: filename_avatar(artist.name))
-        rescue => e
-          Rails.logger.info("Missing Cover for artist #{artist.name} !")
+        # Check if the user is already following the artist. If not, add it to the user's collection of artists
+        unless @current_user.artists.exists?(artist.id)
+          @current_user.artists << artist
         end
       end
     end
+
+    remove_unfollowed_artists(all_followed_artists, previously_followed_artists_by_name)
+
+    # End Timer and print it in console
+    ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    elapsed = ending - starting
+
+    Rails.logger.debug { "Fetching and loading the artists into the DB took #{elapsed.ceil}s" }
   end
 
   private
 
-  # Return a link opeing the artist's page on Spotify's App
+  def remove_unfollowed_artists(all_followed_artists, previously_followed_artists_by_name)
+    followed_artists_names = all_followed_artists.flatten.map(&:name).to_set
+    unfollowed_artist_names = previously_followed_artists_by_name - followed_artists_names
+
+    return if unfollowed_artist_names.empty?
+
+    artists_ids_to_delete = @current_user.artists.where(name: unfollowed_artist_names).pluck(:id)
+    @current_user.artists_collections.where(artist_id: artists_ids_to_delete).destroy_all
+  end
+
+
+  # Use Spotify's API to fetch the following artists
+  def fetch_followed_artists(last_artist_id)
+    @spotify_user.following(type: 'artist', limit: 50, after: last_artist_id)
+  end
+
+  # Attach the avatar to the artist
+  def attach_avatar(artist, spotify_artist)
+    avatar_url = spotify_artist.images.last&.dig("url")
+    if avatar_url
+      begin
+        artist.avatar.attach(io: open_avatar(avatar_url), filename: filename_avatar(artist.name))
+      rescue => e
+        Rails.logger.info("Error attaching avatar for artist #{artist.name}: #{e.message}")
+      end
+    else
+      Rails.logger.info("Missing avatar URL for artist #{artist.name}")
+    end
+  end
+
+  # Return a link opening the artist's page on Spotify's App
   def external_link(artist_url)
     "#{artist_url}?si=QoKxSaCaRyCyG7-dRHDVEg"
   end
---
+++ spec/services/spotify_service_spec.rb
@@ -0,0 +1,31 @@
+RSpec.describe SpotifyService do
+  context "#load_artists" do
+    # let(:spotify_data) {
+    #
+    # }
+
+    # Get Spotify's user data
+    spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+    # Save User's spotify data
+    current_user.update(spotify_data: spotify_user.as_json)
+    # Save artists
+
+
+    subject(:load_artists) { SpotifyService.new(SpotifyService.new(current_user, spotify_user).load_artists) }
+
+    context "when there is a new followed artist" do
+      it "adds a new artist into the database" do
+
+      end
+
+
+      it "adds the artist to the user's collection of artists"
+    end
+
+    context "when a spotify artisthas been unfollowed" do
+      it "removes the link from the user's collection of artists"
+
+      it "keeps the artist into the database"
+    end
+  end
+end
