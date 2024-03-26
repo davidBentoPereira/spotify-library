8ac1ff4406c9985b0cf8271bd9d0e1d68d59d194
Bento <david.bento-pereira@lilo.org>
2024-03-26 15:56:53 +0100

    Refactor SyncFollowedArtistsJob to remove useless syntax "begin"

--- app/sidekiq/sync_followed_artists_job.rb
+++ app/sidekiq/sync_followed_artists_job.rb
@@ -2,13 +2,10 @@ class SyncFollowedArtistsJob
   include Sidekiq::Worker
 
   def perform(user_id)
-    begin
-      user = User.find(user_id)
+    user = User.find(user_id)
 
-      SpotifyService.new(user).load_artists
-    rescue StandardError => e
-      Rails.logger.error "Error syncing followed artists for user #{user_id}: #{e.message}"
-    end
-
+    SpotifyService.new(user).load_artists
+  rescue StandardError => e
+    Rails.logger.error "Error syncing followed artists for user #{user_id}: #{e.message}"
   end
 end
