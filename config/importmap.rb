780f5cec233ea77751ab6b2fdc9340009ba48794
Bento <david.bento-pereira@lilo.org>
2024-04-17 13:39:18 +0200

    Remove useless js import

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,4 +6,3 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "slim-select", to: "https://ga.jspm.io/npm:slim-select@2.8.2/dist/slimselect.js"
-pin "@stimulus-components/dialog", to: "https://ga.jspm.io/npm:@stimulus-components/dialog@1.0.1/dist/stimulus-dialog.mjs"
