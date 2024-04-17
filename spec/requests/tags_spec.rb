1b4c16156cdeda64874d00de45e6b8b64cabd556
Bento <david.bento-pereira@lilo.org>
2024-04-17 19:20:26 +0200

    Add specs for TagsController

--- spec/requests/tags_spec.rb
+++ spec/requests/tags_spec.rb
@@ -1,7 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe TagsController, type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "DELETE /tags/:id" do
+    let(:user) { create(:user, :with_followed_artists) }
+    let(:tag) { user.tags.first }
+
+    context "when user is logged in" do
+      before { sign_in user }
+
+      it "deletes the tag" do
+        delete "/tags/#{tag.id}"
+
+        expect(response).to redirect_to(spotify_followed_artists_path)
+        expect(user.tags.exists?(name: tag.name)).to be_falsey
+      end
+    end
+
+    context "when user is not logged in" do
+      it "does not delete the tag" do
+        delete "/tags/#{tag.id}"
+
+        expect(response).to redirect_to(new_user_session_path)
+        expect(user.tags.exists?(name: tag.name)).to be_truthy
+      end
+    end
   end
 end
